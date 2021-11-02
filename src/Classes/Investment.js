b4ff7f36c2002705aa538e96efb4c6fe594b158b
Dmytro Shymanskyi <dmytro.shymanskyi.dev@gmail.com>
2021-11-02 14:30:11 +0100

    added class for Investment object

---
+++ src/Classes/Investment.js
@@ -0,0 +1,11 @@
+export class Investment {
+    constructor(id, name, amount, currency, rate, startDate, endDate) {
+        this.id = id;
+        this.name = name;
+        this.amount = amount;
+        this.currency = currency;
+        this.rate = rate;
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+}
